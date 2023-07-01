7f57a5c554e198771b4102f75f6d0ec0463003f6
Bruno Salmon <dev.salmonb@gmail.com>
2023-07-01 08:59:04 +0100

    Initial WebFX version

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.memorygame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.memorygame to javafx.fxml;
-    exports com.example.memorygame;
-}
---
+++ webfx-demo-memorygame-application/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.memorygame.application {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires webfx.platform.resource;
+
+    // Exported packages
+    exports com.example.memorygame;
+
+    // Resources packages
+    opens com.example.memorygame;
+    opens com.example.memorygame.images;
+
+    // Provided services
+    provides javafx.application.Application with com.example.memorygame.Main;
+
+}
